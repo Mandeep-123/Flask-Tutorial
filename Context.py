38fb14821f58c2c19f3536ae4bff867b00d94d1f
Mandeep-123 <38450179+Mandeep-123@users.noreply.github.com>
2020-06-08 23:41:11 +0530

    new2

--- Context.py
+++ Context.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 def index():
 	user_agent = request.headers.get('User-Agent')
 	return '<p>Your browser is {}</p>'.format(user_agent)
+@app.route('/new')
+def newfun():
+	return '<a href="www.google.com">Google</a>'
