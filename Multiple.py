48bd146a72eff4eb40c90b4d0f7503b1142df01c
Mandeep-123 <38450179+Mandeep-123@users.noreply.github.com>
2020-06-10 22:13:57 +0530

    Update and rename username.py to Multiple.py

---
+++ Multiple.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import redirect
+from flask import abort
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello connect check"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello checked by {}!".format(name)
+
+@app.route('/redirect')
+def redirecf():
+	return redirect('http://www.example.com')
+
+@app.route('/user/<id>')
+def get_user(id):
+	user = load_user(id)
+	if not user:
+		abort(404)
+	return '<h1>Hello, {}</h1>'.format(user.name)
