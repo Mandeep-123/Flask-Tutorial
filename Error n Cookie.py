564f846620236bdfb9d227aaaa0169e10dfbadc1
Mandeep-123 <38450179+Mandeep-123@users.noreply.github.com>
2020-06-10 23:16:56 +0530

    Correction

--- Error n Cookie.py
+++ Error n Cookie.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import make_response
 app = Flask(__name__)
 @app.route('/')
 def index():
 	return '<h1>Bad Request</h1>', 400
 
-@app.route('/cookie')
+@app.route('/cooki')
 def index1():
 	response = make_response('<h1>This document carries a cookie!</h1>')
 	response.set_cookie('answer', '42')
-	return response 
+	return response
